f0f1ed2839fdb2436e777ce04da3f7e8180d2257
git-savio1.domi <techsavvydominic3@gmail.com>
2023-04-27 23:30:29 +0300

    Completed assignments

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, {useEffect,useState} from "react";
+
+function App(){
+    const [image,setImage]= useState("")
+
+    const url = "https://dog.ceo/api/breeds/image/random"
+
+    useEffect(()=>{
+        fetch(url)
+        .then(r => r.json())
+        .then(data=>{
+            console.log(data.message)
+            setImage(data.message)
+        })
+    },[])
+    if(image===""){
+        return <p>Loading...</p>
+    }
+
+    return(
+        <div>
+            <img src={image} alt="A Random Dog" />
+        </div>
+    )
+
+
+}
+
+export default App;
